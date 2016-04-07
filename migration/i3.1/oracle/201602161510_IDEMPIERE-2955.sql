28e2e169df365ec4e1b5f06a6151be61a149fc9f
Deepak Pansheriya <deepakpansheriya@gmail.com>
2016-04-07 11:18:27 +0530

    IDEMPIERE-2955: Correcting register migration script

--- migration/i3.1/oracle/201602161510_IDEMPIERE-2955.sql
+++ migration/i3.1/oracle/201602161510_IDEMPIERE-2955.sql
@@ -46,5 +46,5 @@ INSERT INTO AD_Val_Rule (AD_Val_Rule_ID,Name,Description,Type,Code,AD_Client_ID,
 UPDATE AD_Column SET AD_Val_Rule_ID=200093,Updated=TO_DATE('2016-03-10 20:35:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=8417
 ;
 
-SELECT register_migration_script('201602161510_IDEMPIERE-2955') FROM dual
+SELECT register_migration_script('201602161510_IDEMPIERE-2955.sql') FROM dual
 ;
