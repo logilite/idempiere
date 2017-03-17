b3a4ab54cf776db0fe35882db7a60ea1e7fc66ad
Deepak Pansheriya <deepakpansheriya@gmail.com>
2017-03-18 00:38:35 +0530

    Making OpenLDAP selection configurable

--- org.adempiere.base/src/org/compiere/model/MSysConfig.java
+++ org.adempiere.base/src/org/compiere/model/MSysConfig.java
@@ -171,6 +171,7 @@ public class MSysConfig extends X_AD_SysConfig
     public static final String WEBSERVICE_Token_Timeout = "WEBSERVICE_Token_Timeout";
 	public static final String WEBSERVICE_LOGIN_RESPONSE_ALL = "WEBSERVICE_LOGIN_RESPONSE_ALL";
 	public static final String WEBSERVICE_TOKEN_CHECK_CLIENT = "WEBSERVICE_TOKEN_CHECK_CLIENT";
+	public static final String LDAP_IS_OPEN_LDAP = "LDAP_IS_OPEN_LDAP";
 	/**
 	 * 	Standard Constructor
 	 *	@param ctx context
