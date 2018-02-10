6fad2de8b44fc195fb5323e234f7c6f2b0a6b390
Deepak Pansheriya <deepakpansheriya@gmail.com>
2018-02-10 19:25:41 +0530

    IDEMPIERE-3196 : Reverting changes as IDEMPIERE-3483 already covering same

--- org.adempiere.base/src/org/compiere/acct/Doc_ProjectIssue.java
+++ org.adempiere.base/src/org/compiere/acct/Doc_ProjectIssue.java
@@ -134,10 +134,8 @@ public class Doc_ProjectIssue extends Doc
 
 		//  Issue Cost
 		BigDecimal cost = null;
-		if (m_issue.getM_InOutLine_ID() != 0){
+		if (m_issue.getM_InOutLine_ID() != 0)
 			cost = getPOCost(as);
-			cost = cost.multiply(m_line.getQty().abs());
-		}
 		else if (m_issue.getS_TimeExpenseLine_ID() != 0)
 			cost = getLaborCost(as);
 		if (cost == null)	//	standard Product Costs
