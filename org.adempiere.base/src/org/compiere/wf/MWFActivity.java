58de5bbf14a7edf9b9afa328e6ccce590d9fd735
Deepak Pansheriya <deepakpansheriya@gmail.com>
2019-03-26 18:47:52 +0530

    Add context/SQL substitution on WF email recipient address

--- org.adempiere.base/src/org/compiere/wf/MWFActivity.java
+++ org.adempiere.base/src/org/compiere/wf/MWFActivity.java
@@ -1143,7 +1143,7 @@ public class MWFActivity extends X_AD_WF_Activity implements Runnable
 				// Recipient Type
 				String recipient = getNode().getEMailRecipient();
 
-				if (recipient.equals(MWFNode.EMAILRECIPIENT_WFResponsible))
+				if (recipient != null && recipient.equals(MWFNode.EMAILRECIPIENT_WFResponsible))
 				{
 					MWFResponsible resp = getResponsible();
 					if (resp.isHuman())
@@ -1175,6 +1175,95 @@ public class MWFActivity extends X_AD_WF_Activity implements Runnable
 				}
 				else
 				{
+					String value = to;
+
+					// from GridField.defaultFromSQLExpression()
+					String defStr = null;
+					if (value != null && value.startsWith("@SQL="))
+					{
+						String sql = value.substring(5); // w/o tag
+						//sql = Env.parseContext(m_vo.ctx, m_vo.WindowNo, sql, false, true);	//	replace variables
+						//hengsin, capture unparseable error to avoid subsequent sql exception
+						sql = Env.parseContext(m_po.getCtx(), 0, sql, false, false); // replace variables
+						if (sql.equals(""))
+						{
+							log.log(Level.WARNING,
+									"(" + m_node.getAttributeName() + ") - SQL variable parse failed: " + value);
+						}
+						else
+						{
+							PreparedStatement stmt = null;
+							ResultSet rs = null;
+							try
+							{
+								stmt = DB.prepareStatement(sql, null);
+								rs = stmt.executeQuery();
+								if (rs.next())
+									defStr = rs.getString(1);
+								else
+								{
+									if (log.isLoggable(Level.INFO))
+										log.log(Level.INFO, "(" + m_node.getEMail() + ") - no Result: " + sql);
+								}
+							}
+							catch (SQLException e)
+							{
+								log.log(Level.WARNING, "(" + m_node.getEMail() + ") " + sql, e);
+							}
+							finally
+							{
+								DB.close(rs, stmt);
+								rs = null;
+								stmt = null;
+							}
+						}
+						if (defStr != null && defStr.length() > 0)
+						{
+							if (log.isLoggable(Level.FINE))
+								log.fine("[SQL] " + m_node.getEMail() + "=" + defStr);
+							value = defStr;
+						}
+					}
+
+					// from this.fillParameter()
+
+					// Value - Constant/Variable
+					String val = value;
+					if (value == null || (value != null && value.length() == 0))
+						val = null;
+					else if (value.startsWith("@") && !value.startsWith("@SQL=") && m_po != null) // we have a variable
+					{
+						// Strip
+						int index = value.indexOf('@');
+						String columnName = value.substring(index + 1);
+						index = columnName.indexOf('@');
+						if (index == -1)
+						{
+							log.warning(m_node.getAttributeName() + " - cannot evaluate=" + value);
+						}
+						columnName = columnName.substring(0, index);
+						index = m_po.get_ColumnIndex(columnName);
+						if (index != -1)
+						{
+							val = m_po.get_ValueAsString(columnName);
+						}
+						else // not a column
+						{
+							// try Env
+							String env = Env.getContext(getCtx(), columnName);
+							if (env.length() == 0)
+							{
+								log.warning(m_node.getAttributeName() + " - not column nor environment =" + columnName
+										+ "(" + value + ")");
+							}
+							else
+								val = env;
+						}
+					} // @variable@
+
+					if (val != null)
+						to = val;
+
 					client.sendEMail(to, subject, message, null);
 					m_emails.add(to);
 				}
