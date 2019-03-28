bf3c212bd2616a6f28f93a43bdd0a88e447da625
Deepak Pansheriya <deepakpansheriya@gmail.com>
2019-03-28 20:21:43 +0530

    Rename Migration Script.

--- migration/i3.1z/oracle/201903261700_Added_Columns_In_AD_Ref_Table.sql
+++ migration/i3.1z/oracle/201903261700_Added_Columns_In_AD_Ref_Table.sql
@@ -38,5 +38,5 @@ INSERT INTO AD_Field (AD_Field_ID,Name,Description,Help,AD_Tab_ID,AD_Column_ID,I
 UPDATE AD_Field SET DisplayLogic='@IsDisplayIdentifier@=N', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_DATE('2019-03-26 17:02:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1266
 ;
 
-SELECT register_migration_script('201903261700_Add_DisplaySQL_And_DisplayIdentifier_Columns_In_AD_Ref_Table.sql') FROM dual
+SELECT register_migration_script('201903261700_Added_Columns_In_AD_Ref_Table.sql') FROM dual
 ;
