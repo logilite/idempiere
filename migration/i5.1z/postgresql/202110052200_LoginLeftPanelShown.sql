ef1067103a06abfdef1503594708ac9e4680066b
dpansheriya <deepakpansheriya@gmail.com>
2021-10-06 12:32:39 +0530

    Configure Login window to show/hide left side panel

---
+++ migration/i5.1z/postgresql/202110052200_LoginLeftPanelShown.sql
@@ -0,0 +1,7 @@
+-- Login_LeftPanel_Shown
+-- Oct 5, 2021 9:43:44 PM IST
+INSERT INTO AD_SysConfig (AD_SysConfig_ID,AD_Client_ID,AD_Org_ID,Created,Updated,CreatedBy,UpdatedBy,IsActive,Name,Value,Description,EntityType,ConfigurationLevel,AD_SysConfig_UU) VALUES (200185,0,0,TO_TIMESTAMP('2021-10-05 21:43:43','YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2021-10-05 21:43:43','YYYY-MM-DD HH24:MI:SS'),100,100,'Y','ZK_LOGIN_LEFTPANEL_SHOWN','Y','Login window show left side panel','D','S','29335a99-23a6-4ae9-bcb1-967d13c6ecda')
+;
+
+SELECT register_migration_script('202110052200_LoginLeftPanelShown.sql') FROM dual
+;
--- org.adempiere.base/src/org/compiere/model/MSysConfig.java
+++ org.adempiere.base/src/org/compiere/model/MSysConfig.java
@@ -201,6 +201,7 @@ public class MSysConfig extends X_AD_SysConfig
 	public static final String ZK_THEME_USE_FONT_ICON_FOR_IMAGE = "ZK_THEME_USE_FONT_ICON_FOR_IMAGE";
 	public static final String ZK_ALLOW_ROW_SELECTION_IN_MULTIPLE_GRID_PAGES = "ZK_ALLOW_ROW_SELECTION_IN_MULTIPLE_GRID_PAGES";
 	public static final String MULTISELECT_DEFAULT_LOOKUP_OPERATOR = "MULTISELECT_DEFAULT_LOOKUP_OPERATOR";
+	public static final String ZK_LOGIN_LEFTPANEL_SHOWN = "ZK_LOGIN_LEFTPANEL_SHOWN";
 
 	/**
 	 * 	Standard Constructor
