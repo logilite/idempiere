cf16a1ff04ac438e4b87d8f00f7e9036cb2f8bd5
dpansheriya <deepakpansheriya@gmail.com>
2022-07-13 14:30:45 +0530

    Resolving conflict with 202001180340_IDEMPIERE-4651.sql

--- migration/i5.1z/postgresql/202205121530_MutilSelectSearch_Reference.sql
+++ migration/i5.1z/postgresql/202205121530_MutilSelectSearch_Reference.sql
@@ -51,8 +51,5 @@ UPDATE AD_Field SET DisplayLogic=DisplayLogic || ' | @AD_Reference_ID@=200205', 
 UPDATE AD_Field SET DisplayLogic=DisplayLogic || ' | @AD_Reference_ID@=200205', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_TIMESTAMP('2022-05-16 23:09:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=201623
 ;
 
-INSERT INTO AD_SysConfig (AD_SysConfig_ID,AD_Client_ID,AD_Org_ID,Created,Updated,CreatedBy,UpdatedBy,IsActive,Name,Value,Description,EntityType,ConfigurationLevel,AD_SysConfig_UU) VALUES (200168,0,0,TO_TIMESTAMP('2021-01-18 11:13:51','YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2021-01-18 11:13:51','YYYY-MM-DD HH24:MI:SS'),100,100,'Y','ZK_SEARCH_AUTO_COMPLETE_MAX_ROWS','500','Maximum number of rows to retrieve and display for search field''s autocomplete list','D','C','a54d5000-57db-42f7-a910-06ea1e1fd245')
-;
-
 SELECT register_migration_script('202205121530_MutilSelectSearch_Reference.sql') FROM dual
 ;
