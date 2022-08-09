4ae9368939e55ff270d8760574cafd80a1312cc3
dpansheriya <deepakpansheriya@gmail.com>
2022-08-09 09:42:22 +0530

    IDEMPIERE-5370: On Attribute Window, show reference key and dynamic validation field for chosen multiple type

---
+++ migration/i7.1z/oracle/202208051700_IDEMPIERE-5370.sql
@@ -0,0 +1,14 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- On Attribute Window, show reference key and dynamic validation field for chosen multiple type
+-- Aug 5, 2022, 4:33:03 PM IST
+UPDATE AD_Field SET DisplayLogic='@AD_Reference_ID@=17 | @AD_Reference_ID@=18 | @AD_Reference_ID@=19 | @AD_Reference_ID@=30 | @AD_Reference_ID@=200161 | @AD_Reference_ID@=200162 | @AD_Reference_ID@=200163 & @AttributeValueType@=''R''', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_DATE('2022-08-05 16:33:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=206510
+;
+
+-- Aug 5, 2022, 4:33:16 PM IST
+UPDATE AD_Field SET DisplayLogic='@AD_Reference_ID@=17 | @AD_Reference_ID@=18 | @AD_Reference_ID@=30 | @AD_Reference_ID@=200161 | @AD_Reference_ID@=200162 | @AD_Reference_ID@=200163 & @AttributeValueType@=''R''', AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, IsToolbarButton=NULL,Updated=TO_DATE('2022-08-05 16:33:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=204144
+;
+
+SELECT register_migration_script('202208051700_IDEMPIERE-5370.sql') FROM dual
+;
