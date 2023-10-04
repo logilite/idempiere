f451b85c15795f84bce17823ae2bcd2aa4652ae1
dpansheriya <deepakpansheriya@gmail.com>
2023-10-04 17:53:21 +0530

    IDEMPIERE-5836 : Adding callout to mark expense type item as default to stock N

--- migration/i7.1z/postgresql/202309291327_IDEMPIERE-5836.sql
+++ migration/i7.1z/postgresql/202309291327_IDEMPIERE-5836.sql
@@ -8,3 +8,6 @@ UPDATE AD_Field SET DisplayLogic='@IsSummary@=''N'' & @ProductType@=I | @Product
 UPDATE AD_Column SET ReadOnlyLogic='@ProductType@=R | @ProductType@=O',Updated=TO_TIMESTAMP('2023-08-08 13:29:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1761
 ;
 
+-- Oct 2, 2023, 3:38:33 PM IST
+UPDATE AD_Column SET Callout='org.compiere.model.CalloutProduct.productType',Updated=TO_TIMESTAMP('2023-10-02 15:38:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=7795
+;
