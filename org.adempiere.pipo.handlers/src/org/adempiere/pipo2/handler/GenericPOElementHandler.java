42f76a70aa276007a8aca97fec0a41f62078379b
dpansheriya <deepakpansheriya@gmail.com>
2023-11-26 17:47:15 +0530

    Fix merging issue in v9

--- org.adempiere.pipo.handlers/src/org/adempiere/pipo2/handler/GenericPOElementHandler.java
+++ org.adempiere.pipo.handlers/src/org/adempiere/pipo2/handler/GenericPOElementHandler.java
@@ -280,15 +280,14 @@ public class GenericPOElementHandler extends AbstractElementHandler {
 					}
 					
 					ctx.packOut.getCtx().ctx.put("Table_Name", mainTable);
-					try
-					{
-						ElementHandler handler = ctx.packOut.getHandler(I_AD_TableAttribute.Table_Name);
-						handler.packOut(ctx.packOut, document, null, po.get_ID());
-					}
-					catch (Exception e)
-					{
-						if (log.isLoggable(Level.INFO))
-							log.info(e.toString());
+					if (po.get_ID() > 0) {
+						try {
+							handler = ctx.packOut.getHandler(I_AD_TableAttribute.Table_Name);
+							handler.packOut(ctx.packOut, document, null, po.get_ID());
+						} catch (Exception e) {
+							if (log.isLoggable(Level.INFO))
+								log.info(e.toString());
+						}
 					}
 				}
 				for (int i=1; i<tables.length; i++) {
