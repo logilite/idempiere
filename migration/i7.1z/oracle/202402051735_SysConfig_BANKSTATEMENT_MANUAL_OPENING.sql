92b050fae8741d1778ac7acb08324c2d4db628bc
dpansheriya <deepakpansheriya@gmail.com>
2024-02-05 18:13:28 +0530

    Making Bank Statement begining / opening balance manual vs auto as system configurable.

---
+++ migration/i7.1z/oracle/202402051735_SysConfig_BANKSTATEMENT_MANUAL_OPENING.sql
@@ -0,0 +1,10 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Adding System Configurator BANKSTATEMENT MANUAL OPENING
+-- 05-Feb-2024, 5:32:12 PM IST
+INSERT INTO AD_SysConfig (AD_SysConfig_ID,AD_Client_ID,AD_Org_ID,Created,Updated,CreatedBy,UpdatedBy,IsActive,Name,Value,EntityType,ConfigurationLevel,AD_SysConfig_UU) VALUES (200243,0,0,TO_DATE('2024-02-05 17:32:00','YYYY-MM-DD HH24:MI:SS'),TO_DATE('2024-02-05 17:32:00','YYYY-MM-DD HH24:MI:SS'),0,0,'Y','BANKSTATEMENT_MANUAL_OPENING','N','D','C','eb84d0f0-a9a4-4cb0-b173-918ba714034d')
+;
+
+SELECT register_migration_script('202402051735_SysConfig_BANKSTATEMENT_MANUAL_OPENING.sql') FROM dual
+;
