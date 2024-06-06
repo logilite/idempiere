50aa733d0b072073bd5ccd25295faea23fa00cf8
dpansheriya <deepakpansheriya@gmail.com>
2024-06-06 16:41:05 +0530

    IDEMPIERE-6155 : Fixing migration issue

--- migration/i7.1z/postgresql/202405231900_IDEMPIERE-6155.sql
+++ migration/i7.1z/postgresql/202405231900_IDEMPIERE-6155.sql
@@ -138,7 +138,7 @@ UPDATE AD_Field SET SeqNo=135, AD_Reference_Value_ID=NULL, AD_Val_Rule_ID=NULL, 
 
 -- Update Reference Id for Invoice Parameter of Issue Project Process
 -- Jun 5, 2024, 7:03:56 PM IST
-UPDATE AD_Process_Para SET AD_Reference_ID=30,Updated=TO_TIMESTAMP('2024-06-05 19:03:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1000058
+UPDATE AD_Process_Para SET AD_Reference_ID=30,Updated=TO_TIMESTAMP('2024-06-05 19:03:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=200483
 ;
 
 SELECT register_migration_script('202405231900_IDEMPIERE-6155.sql') FROM dual
