517e296ada39c1dfce3c6321dccd5ab46c70ad67
dpansheriya <deepakpansheriya@gmail.com>
2024-11-23 22:08:30 +0530

    IDEMPIERE-5653 : Fixing issue of Print format access records not visible, after sharing print foramt getting NPE

---
+++ migration/i7.1z/postgresql/202411221055_IDEMPIERE-5653.sql
@@ -0,0 +1,7 @@
+-- IDEMPIERE-5653: Print format access and restriction
+-- 23-Nov-2024, 10:01:14 PM IST
+UPDATE AD_Tab SET WhereClause=NULL, DisplayLogic='@#ShowAdvanced@=Y',Updated=TO_TIMESTAMP('2024-11-23 22:01:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=200357
+;
+
+SELECT register_migration_script('202411221055_IDEMPIERE-5653.sql') FROM dual
+;
