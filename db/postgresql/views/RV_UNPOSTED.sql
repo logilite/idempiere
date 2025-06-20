e1fc4727b06fae6cd301454ff30e86983e81a752
Sachin Bhimani <sbhimani@logilite.com>
2025-06-20 20:05:33 +0530

    RV_UNPOSTED View Formatted

--- db/postgresql/views/RV_UNPOSTED.sql
+++ db/postgresql/views/RV_UNPOSTED.sql
@@ -1,101 +1,354 @@
-create or replace view rv_unposted
-as
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, DateDoc, DateAcct, 224 AS AD_Table_ID,
-          GL_Journal_ID AS Record_ID, 'N' AS IsSOTrx, posted, processing,
-          processed, docstatus, processedon
-     from GL_JOURNAL
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT pi.AD_Client_ID, pi.AD_Org_ID, pi.Created, pi.CreatedBy, pi.Updated,
-          pi.UpdatedBy, pi.IsActive, p.NAME || '_' || pi.Line,
-          pi.MovementDate, pi.MovementDate, 623, pi.C_ProjectIssue_ID, 'N',
-          posted, pi.processing, pi.processed, 'CO' as DocStatus, processedon
-     from C_PROJECTISSUE pi INNER JOIN C_PROJECT p
-          ON (pi.C_Project_ID = p.C_Project_ID)
-    WHERE Posted <> 'Y'                                  --AND DocStatus<>'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, DateInvoiced, DateAcct, 318, C_Invoice_ID,
-          IsSOTrx, posted, processing, processed, docstatus, processedon
-     from C_INVOICE
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, MovementDate, DateAcct, 319, M_InOut_ID,
-          IsSOTrx, posted, processing, processed, docstatus, processedon
-     from M_INOUT
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, MovementDate, MovementDate, 321,
-          M_Inventory_ID, 'N', posted, processing, processed, docstatus, processedon
-     from M_INVENTORY
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, MovementDate, MovementDate, 323,
-          M_Movement_ID, 'N', posted, processing, processed, docstatus, processedon
-     from M_MOVEMENT
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, documentno, MovementDate, MovementDate, 325, M_Production_ID,
-          'N', posted, processing, processed, docstatus, processedon
-     from M_PRODUCTION
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'                                
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, NAME, StatementDate, DateAcct, 407, C_Cash_ID, 'N',
-          posted, processing, processed, docstatus, processedon
-     from C_CASH
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, DateTrx, DateAcct, 335, C_Payment_ID, 'N',
-          posted, processing, processed, docstatus, processedon
-     from C_PAYMENT
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, DateTrx, DateAcct, 735, C_AllocationHdr_ID,
-          'N', posted, processing, processed, docstatus, processedon
-     from C_ALLOCATIONHDR
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, NAME, StatementDate, StatementDate, 392,
-          C_BankStatement_ID, 'N', posted, processing, processed, docstatus, processedon
-     from C_BANKSTATEMENT
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, DateTrx, DateAcct, 472, M_MatchInv_ID, 'N',
-          posted, processing, processed, 'CO' as docstatus, processedon
-     from M_MATCHINV
-    WHERE Posted <> 'Y' AND COALESCE(M_MatchInvHdr_ID, 0) = 0                                 --AND DocStatus<>'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, DateTrx, DateAcct, 473, M_MatchPO_ID, 'N',
-          posted, processing, processed, 'CO' as docstatus, processedon
-     from M_MATCHPO
-    WHERE Posted <> 'Y'                                  --AND DocStatus<>'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, DateOrdered, DateAcct, 259, C_Order_ID,
-          IsSOTrx, posted, processing, processed, docstatus, processedon
-     from C_ORDER
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, DateDoc, DateDoc, 702,
-          M_Requisition_ID, 'N', posted, processing, processed, docstatus, processedon
-     from M_REQUISITION
-    WHERE Posted <> 'Y' AND DocStatus <> 'VO'
-   UNION
-   SELECT AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy,
-          IsActive, DocumentNo, DateTrx, DateAcct, 200196, M_MatchInvHdr_ID, 'N',
-          Posted, Processing, Processed, DocStatus, Processedon
-     FROM M_MatchInvHdr
-    WHERE Posted <> 'Y'
-;
+ CREATE OR replace VIEW rv_unposted
+AS
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         datedoc,
+         dateacct,
+         224           AS AD_Table_ID,
+         gl_journal_id AS Record_ID,
+         'N'           AS IsSOTrx,
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   gl_journal
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT pi.ad_client_id,
+         pi.ad_org_id,
+         pi.created,
+         pi.createdby,
+         pi.updated,
+         pi.updatedby,
+         pi.isactive,
+         p.name
+         || '_'
+         || pi.line,
+         pi.movementdate,
+         pi.movementdate,
+         623,
+         pi.c_projectissue_id,
+         'N',
+         posted,
+         pi.processing,
+         pi.processed,
+         'CO' AS DocStatus,
+         processedon
+  FROM   c_projectissue pi
+         inner join c_project p
+                 ON ( pi.c_project_id = p.c_project_id )
+  WHERE  posted <> 'Y' --AND DocStatus<>'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         dateinvoiced,
+         dateacct,
+         318,
+         c_invoice_id,
+         issotrx,
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   c_invoice
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         movementdate,
+         dateacct,
+         319,
+         m_inout_id,
+         issotrx,
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   m_inout
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         movementdate,
+         movementdate,
+         321,
+         m_inventory_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   m_inventory
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         movementdate,
+         movementdate,
+         323,
+         m_movement_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   m_movement
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         movementdate,
+         movementdate,
+         325,
+         m_production_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   m_production
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         name,
+         statementdate,
+         dateacct,
+         407,
+         c_cash_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   c_cash
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         datetrx,
+         dateacct,
+         335,
+         c_payment_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   c_payment
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         datetrx,
+         dateacct,
+         735,
+         c_allocationhdr_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   c_allocationhdr
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         name,
+         statementdate,
+         statementdate,
+         392,
+         c_bankstatement_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   c_bankstatement
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         datetrx,
+         dateacct,
+         472,
+         m_matchinv_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         'CO' AS docstatus,
+         processedon
+  FROM   m_matchinv
+  WHERE  posted <> 'Y'
+         AND Coalesce(m_matchinvhdr_id, 0) = 0 --AND DocStatus<>'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         datetrx,
+         dateacct,
+         473,
+         m_matchpo_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         'CO' AS docstatus,
+         processedon
+  FROM   m_matchpo
+  WHERE  posted <> 'Y' --AND DocStatus<>'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         dateordered,
+         dateacct,
+         259,
+         c_order_id,
+         issotrx,
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   c_order
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         datedoc,
+         datedoc,
+         702,
+         m_requisition_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   m_requisition
+  WHERE  posted <> 'Y'
+         AND docstatus <> 'VO'
+  UNION
+  SELECT ad_client_id,
+         ad_org_id,
+         created,
+         createdby,
+         updated,
+         updatedby,
+         isactive,
+         documentno,
+         datetrx,
+         dateacct,
+         200196,
+         m_matchinvhdr_id,
+         'N',
+         posted,
+         processing,
+         processed,
+         docstatus,
+         processedon
+  FROM   m_matchinvhdr
+  WHERE  posted <> 'Y';  
