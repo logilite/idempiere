e68a72f4900cf53882218c4f244003f6825e0452
Sachin Bhimani <sbhimani@logilite.com>
2025-07-03 18:20:11 +0530

    IDEMPIERE-4224: Add validation to prevent duplicate attribute names when updating an attribute used in Table Attribute Set Type

--- org.adempiere.base/src/org/compiere/model/MAttribute.java
+++ org.adempiere.base/src/org/compiere/model/MAttribute.java
@@ -30,6 +30,7 @@ import org.compiere.util.DB;
 import org.compiere.util.Env;
 import org.compiere.util.KeyNamePair;
 import org.compiere.util.Msg;
+import org.compiere.util.Util;
 
 /**
  *  Product Attribute
@@ -48,7 +49,12 @@ public class MAttribute extends X_M_Attribute
 	private static CLogger s_log = CLogger.getCLogger (MAttribute.class);
 
 	private static CCache<Integer, MAttribute>	s_cache				= new CCache<Integer, MAttribute>(Table_Name, 30, 60);
-	
+
+	private static final String			SQL_GET_TA_DUPLICATE_ATTRIB	= " SELECT st.Name FROM M_Attribute a "
+																	+ " INNER JOIN M_AttributeUse u  ON (u.M_Attribute_ID = a.M_Attribute_ID) "
+																	+ " INNER JOIN M_AttributeSet st ON (st.M_AttributeSet_ID = u.M_AttributeSet_ID AND st.M_AttributeSet_Type = 'TA' ) "
+																	+ " WHERE a.AD_Client_ID IN (0, ?) AND UPPER(a.Name) = UPPER(?) ";
+
 	/**	Values						*/
 	private MAttributeValue[]		m_values = null;
 
@@ -312,6 +318,16 @@ public class MAttribute extends X_M_Attribute
 				&& ! MRole.getDefault().isAccessAdvanced()) {
 			log.saveError("Error", Msg.getMsg(getCtx(), "ActionNotAllowedHere"));
 			return false;
+		}
+
+		if (!newRecord && is_ValueChanged(COLUMNNAME_Name))
+		{
+			String dupAttribSetName = DB.getSQLValueString(get_TrxName(), SQL_GET_TA_DUPLICATE_ATTRIB, getAD_Client_ID(), getName());
+			if (!Util.isEmpty(dupAttribSetName, true))
+			{
+				log.saveError("Error", Msg.getMsg(getCtx(), "UniqueAttribute", new Object[] { getName(), dupAttribSetName }));
+				return false;
+			}
 		}
 		return true;
 	}
