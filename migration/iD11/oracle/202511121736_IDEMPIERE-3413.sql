79a059cfd13e557e50c210664f492b45578f8676
Sachin Bhimani <sbhimani@logilite.com>
2025-11-12 17:59:56 +0530

    IDEMPIERE-3413: Multi-Select Default value support

---
+++ migration/iD11/oracle/202511121736_IDEMPIERE-3413.sql
@@ -0,0 +1,10 @@
+-- IDEMPIERE-3413 Multi-Select Default Value support
+SELECT register_migration_script('202511121736_IDEMPIERE-3413.sql') FROM dual;
+
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+-- Nov 12, 2025, 5:36:36 PM IST
+UPDATE AD_Field SET DisplayLogic=NULL,Updated=TO_TIMESTAMP('2025-11-12 17:36:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=160
+;
+
