fe5d90b665e3dd6fa0dee6c175163489d4190157
pambani <pambani@logilite.com>
2026-03-13 19:23:37 +0530

    IDEMPIERE-6824 Enhance error messages to make them clearer and more user friendly

--- migration/iD11/postgresql/202602261445_IDEMPIERE-6824.sql
+++ migration/iD11/postgresql/202602261445_IDEMPIERE-6824.sql
@@ -2,6 +2,6 @@
 SELECT register_migration_script('202602261445_IDEMPIERE-6824.sql') FROM dual;
 
 -- 26/02/2026 14:45:30 IST
-INSERT INTO AD_Message (MsgType,MsgText,MsgTip,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('E','Role cannot be unmarked as Master Role because it has Included Roles records.','Remove all Included Roles before unmarking this role as Master.',0,0,'Y',TO_TIMESTAMP('2026-02-26 14:45:29','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2026-02-26 14:45:29','YYYY-MM-DD HH24:MI:SS'),100,200991,'RoleHasIncludedRoles','D','4d5a0471-aed5-4e1b-8e35-18ae51e77a88')
+INSERT INTO AD_Message (MsgType,MsgText,MsgTip,AD_Client_ID,AD_Org_ID,IsActive,Created,CreatedBy,Updated,UpdatedBy,AD_Message_ID,Value,EntityType,AD_Message_UU) VALUES ('E','This role cannot be unmarked as a Master Role because it is currently included in the following roles: {0}.','All included role references must be removed before this role can be unmarked as Master.',0,0,'Y',TO_TIMESTAMP('2026-02-26 14:45:29','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2026-02-26 14:45:29','YYYY-MM-DD HH24:MI:SS'),100,200991,'RoleHasIncludedRoles','D','4d5a0471-aed5-4e1b-8e35-18ae51e77a88')
 ;
 
