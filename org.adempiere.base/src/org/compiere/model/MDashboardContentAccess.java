33a586297bbbbd5568fa998ea93a9d1653dbe7f4
pambani <pambani@logilite.com>
2026-06-23 15:49:04 +0530

    IDEMPIERE-7044: Updated restrict access validation to consider included roles - corrected query for role access evaluation

--- org.adempiere.base/src/org/compiere/model/MDashboardContentAccess.java
+++ org.adempiere.base/src/org/compiere/model/MDashboardContentAccess.java
@@ -101,7 +101,10 @@ public class MDashboardContentAccess extends X_PA_DashboardContent_Access {
 		sql.append(" AND PA_DashboardContent_ID NOT IN (SELECT PA_DashboardContent_ID FROM PA_DashboardContent_Access ct2 WHERE ct2.IsActive='N' AND ct2.AD_Client_ID in (0,?)");
 		parameters.add(AD_Client_ID);
 		if (AD_Role >= 0) {
-			sql.append(" AND COALESCE(ct2.AD_Role_ID, ?) = ?");			  
+			sql.append(" AND (	COALESCE(ct2.AD_Role_ID, ?) = ?");
+			sql.append("		OR COALESCE(ct2.AD_Role_ID, ?) IN (SELECT  Included_Role_ID FROM AD_Role_Included WHERE AD_Role_ID = ?))");
+			parameters.add(AD_Role);
+			parameters.add(AD_Role);
 			parameters.add(AD_Role);
 			parameters.add(AD_Role);
 		}
@@ -120,8 +123,12 @@ public class MDashboardContentAccess extends X_PA_DashboardContent_Access {
 		   .append(" WHERE cta.IsActive='Y'")
 		   .append(" AND ct.IsActive='Y'");
 				
-		if(AD_Role >= 0) {
-			sql.append(" AND COALESCE(cta.AD_Role_ID, ?) = ?");			  
+		if (AD_Role >= 0)
+		{
+			sql.append(" AND (	COALESCE(cta.AD_Role_ID, ?) = ?");
+			sql.append("		OR COALESCE(cta.AD_Role_ID, ?) IN (SELECT  Included_Role_ID FROM AD_Role_Included WHERE AD_Role_ID = ?))");
+			parameters.add(AD_Role);
+			parameters.add(AD_Role);
 			parameters.add(AD_Role);
 			parameters.add(AD_Role);
 		}
